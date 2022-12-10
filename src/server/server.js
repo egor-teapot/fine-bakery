e25488e8aff02de28d90bde3c0c6ee7ebc576540
laptop-3 <schodik666@gmail.com>
2022-12-10 15:32:44 +0300

    main-9

--- src/server/server.js
+++ src/server/server.js
@@ -4,15 +4,19 @@ const PORT = 4127
 const path = require('path')
 const views = path.join(__dirname, '../client/views/')
 
+const loginRoute = require('./routes/login')
+const dbRoute = require('./db/db')
+
 app.use('/css', express.static(path.join(__dirname, '../../node_modules/bootstrap/dist/css')))
+app.use(express.json())
+app.use(express.urlencoded())
 
 app.get('/', (req, res) => {
     res.sendFile(views + 'root.html')
 })
 
-app.get('/login', (req, res) => {
-    res.sendFile(views + 'login-register/login.html')
-})
+app.use('/login', loginRoute)
+app.use('/api/db', dbRoute)
 
 app.listen(PORT, () => {
     console.log(`server runnig on port ${PORT}`)
